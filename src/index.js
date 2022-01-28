8b8e942bf39288ec77894d9ba170065b520778d3
6ajker <bajker_mexx@mail.ru>
2022-01-28 14:18:54 +0300

    'continue'

--- src/index.js
+++ src/index.js
@@ -10,11 +10,16 @@ module.exports = function check(str, bracketsConfig) {
     return true;
   }
   for (let i = 0; i < str.length; i++) {
-    if (str[i] ) {
-
+    if (isClose(str[i])) {
+      if (brackets[str[i]] !== stack.pop()) {
+        return false;
+      }
     } else {
-      
+      stack.push(str[i])
     }
   }
   return !stack.length
+  function isClose(item) {
+    return [')', '}', ']', '|'].indexOf(item) > -1;
+  }
 }
