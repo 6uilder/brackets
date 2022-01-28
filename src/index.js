dc4bf4c822fc31a87b457045aa18174dd48a3ca9
6ajker <bajker_mexx@mail.ru>
2022-01-28 13:53:11 +0300

    'start'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let brackets = {
+    ')': '(',
+    '}': '{',
+    ']': '[',
+    '|': '|'
+  }
+  if (str.length === 0) {
+    return true;
+  }
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] ) {
+
+    } else {
+      
+    }
+  }
+  return !stack.length
 }
