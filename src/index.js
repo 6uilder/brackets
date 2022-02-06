92ebd87e268e1238669b5348154181e18248ecb0
6ajker <bajker_mexx@mail.ru>
2022-02-06 16:48:18 +0300

    'feat:OK'

--- src/index.js
+++ src/index.js
@@ -1,25 +1,87 @@
-module.exports = function check(str, bracketsConfig) {
+module.exports = function check(str) {
   let stack = [];
   let brackets = {
     ')': '(',
     '}': '{',
     ']': '[',
-    '|': '|'
+    '2': '1',
+    '4': '3',
+    '6': '5'
   }
+  let countStick = 0;
+  let countSeven = 0;
+  let countEight = 0;
+  let mm = 'some';
+  let tf = 'some';
   if (str.length === 0) {
     return true;
   }
   for (let i = 0; i < str.length; i++) {
-    if (isClose(str[i])) {
-      if (brackets[str[i]] !== stack.pop()) {
+    if (str[i]==='|') {
+      Stick(str[i]);
+	  if (tf == false) {
+		  return false
+	  }
+      continue;
+    }
+	if (str[i]==='7') {
+      Seven(str[i]);
+	  if (tf == false) {
+		return false
+	  }
+      continue;
+    }
+	if (str[i]==='8') {
+      Eight(str[i]);
+	  	  if (tf == false) {
+		  return false
+	  }
+      continue;
+    }
+    if (isClose(str[i])) { // если скобочка закрывающаяся
+      if (brackets[str[i]] !== stack.pop()) {//не равен последней в стеке
         return false;
       }
-    } else {
+    } else { //кладем в стек, если скобочка открывающаяся
       stack.push(str[i])
     }
   }
-  return !stack.length
+  
+  return stack.length === 0
+  
   function isClose(item) {
-    return [')', '}', ']', '|'].indexOf(item) > -1;
-  }
+    return [')', '}', ']', '2', '4', '6'].indexOf(item) > -1;
+  } 
+
+  function Stick(item) {
+	  ++countStick;
+    if (countStick % 2 !== 0) {
+		
+      stack.push(item)
+    } else {
+      if (item !== stack.pop()) {//не равен последней в стеке
+        tf =  false;
+    }
+  }}
+  
+    function Seven(item) {
+	  ++countSeven;
+    if (countSeven % 2 !== 0) {
+      stack.push(item)
+    } else {
+      if (item !== stack.pop()) {//не равен последней в стеке
+        tf =  false;
+    }
+  }}
+    function Eight(item) {
+	  ++countEight;
+    if (countEight % 2 !== 0) {
+      stack.push(item)
+    } else {
+      if (item !== stack.pop()) {//не равен последней в стеке
+        tf =  false;
+    }
+  }}
+
 }
+
